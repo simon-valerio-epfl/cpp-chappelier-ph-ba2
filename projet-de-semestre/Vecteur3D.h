cc5f5e0924676276129af9e5a2e41223df5d51df
Androz 2091 <androz2091@gmail.com>
2024-02-22 22:46:15 +0100

    fix formatting

--- projet-de-semestre/Vecteur3D.h
+++ projet-de-semestre/Vecteur3D.h
@@ -1,11 +1,12 @@
-# pragma once
+#pragma once
 
 class Vecteur3D
 {
-    private:
-        double x, y, z;
-    public:
-        void set_coord(int idx, double value);
-        void affiche();
-        bool compare(Vecteur3D v);
+private:
+    double x, y, z;
+
+public:
+    void set_coord(int idx, double value);
+    void affiche();
+    bool compare(Vecteur3D v);
 };
--- projet-de-semestre/testVecteur3D.cc
+++ projet-de-semestre/testVecteur3D.cc
@@ -5,11 +5,12 @@ Vecteur3D vect1;
 Vecteur3D vect2;
 Vecteur3D vect3;
 
-int main() {
+int main()
+{
 
     /* Cette partie sera revue dans 2 semaines
-    * (constructeurs, surcharge des opérateurs).
-    */
+     * (constructeurs, surcharge des opérateurs).
+     */
     // v1 = (1.0, 2.0, -0.1)
     vect1.set_coord(0, 1.0);
     vect1.set_coord(1, 2.0);
@@ -18,7 +19,7 @@ int main() {
     // v2 = (2.6, 3.5,  4.1)
     vect2.set_coord(0, 2.6);
     vect2.set_coord(1, 3.5);
-    vect2.set_coord(2, 4.1); 
+    vect2.set_coord(2, 4.1);
 
     vect3 = vect1;
 
@@ -31,17 +32,23 @@ int main() {
     std::cout << std::endl;
 
     std::cout << "Le vecteur 1 est ";
-    if (vect1.compare(vect2)) {
+    if (vect1.compare(vect2))
+    {
         std::cout << "égal au";
-    } else {
+    }
+    else
+    {
         std::cout << "différent du";
     }
-    std::cout << " vecteur 2," << std::endl << "et est ";
-    if (not vect1.compare(vect3)) {
+    std::cout << " vecteur 2," << std::endl
+              << "et est ";
+    if (not vect1.compare(vect3))
+    {
         std::cout << "différent du";
-    } else {
+    }
+    else
+    {
         std::cout << "égal au";
     }
     std::cout << " vecteur 3." << std::endl;
-
 }
