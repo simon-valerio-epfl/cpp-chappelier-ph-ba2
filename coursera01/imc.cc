83d80f765e872d9c93d978b4414a037cab4425a9
Androz 2091 <androz2091@gmail.com>
2024-02-25 10:33:13 +0100

    add coursera

---
+++ coursera01/imc.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+/*****************************************************
+ * Compléter le code à partir d'ici
+ *****************************************************/
+class Patient
+{
+private:
+	double masse;
+	double hauteur;
+
+public:
+	void init(double poids_initial, double taille_initiale)
+	{
+		if (poids_initial < 0 or taille_initiale < 0)
+		{
+			masse = 0;
+			hauteur = 0;
+		}
+		else
+		{
+			masse = poids_initial;
+			hauteur = taille_initiale;
+		}
+	}
+	void afficher() const
+	{
+		cout << "Patient :" << masse << "kg pour " << hauteur << "m" << endl;
+	}
+	double poids() const
+	{
+		return masse;
+	}
+	double taille() const
+	{
+		return hauteur;
+	}
+	double imc() const
+	{
+		return hauteur == 0 ? 0 : masse / (hauteur * hauteur);
+	}
+};
+/*******************************************
+ * Ne rien modifier après cette ligne.
+ *******************************************/
+
+int main()
+{
+	Patient quidam;
+	double poids, taille;
+	do
+	{
+		cout << "Entrez un poids (kg) et une taille (m) : ";
+		cin >> poids >> taille;
+		quidam.init(poids, taille);
+		quidam.afficher();
+		cout << "IMC : " << quidam.imc() << endl;
+	} while (poids * taille != 0.0);
+	return 0;
+}
---
+++ coursera01/tirelire.cc
@@ -0,0 +1,128 @@
+#include <iostream>
+using namespace std;
+
+/*******************************************
+ * Complétez le programme à partir d'ici.
+ *******************************************/
+class Tirelire
+{
+	double montant;
+
+public:
+	double getMontant()
+	{
+		return montant;
+	}
+	void afficher()
+	{
+		string etat = montant <= 0 ? " Vous etes sans le sou." : " Vous avez : " + to_string((int) montant) + " euros dans votre tirelire.";
+		cout << etat << endl;
+	}
+	void secouer()
+	{
+		if (montant > 0)
+		{
+			cout << " Bing bing " << endl;
+		}
+	}
+	void remplir(double argent_entrant)
+	{
+		if (argent_entrant > 0)
+		{
+			montant = argent_entrant; // on part du principe que la tirelire est vide //
+		}
+	}
+	void vider()
+	{
+		montant = 0;
+	}
+	void puiser(double argent_demande)
+	{
+		if (argent_demande > montant)
+		{
+			montant = 0;
+		}
+		else if (argent_demande > 0)
+		{
+			montant = montant - argent_demande;
+		}
+	}
+	bool montant_suffisant(double budget, double &solde)
+	{
+		bool suffisant;
+		if (budget <= 0)
+		{
+			suffisant = true;
+			solde = montant;
+		}
+		else if (budget <= montant)
+		{
+			suffisant = true;
+			solde = montant - budget;
+		}
+		else
+		{
+			suffisant = false;
+			solde = budget - montant;
+		}
+		return suffisant;
+	}
+	double calculerSolde(double budget)
+	{
+		return montant - budget;
+	}
+};
+
+/*******************************************
+ * Ne rien modifier après cette ligne.
+ *******************************************/
+
+int main()
+{
+	Tirelire piggy;
+
+	piggy.vider();
+	piggy.secouer();
+	piggy.afficher();
+
+	piggy.puiser(20.0);
+	piggy.secouer();
+	piggy.afficher();
+
+	piggy.remplir(550.0);
+	piggy.secouer();
+	piggy.afficher();
+
+	piggy.puiser(10.0);
+	piggy.puiser(5.0);
+	piggy.afficher();
+
+	cout << endl;
+
+	// le budget de vos vacances de rève.
+	double budget;
+
+	cout << "Donnez le budget de vos vacances : ";
+	cin >> budget;
+
+	// ce qui resterait dans la tirelire après les
+	// vacances
+	double solde(0.0);
+
+	if (piggy.montant_suffisant(budget, solde))
+	{
+		cout << "Vous êtes assez riche pour partir en vacances !"
+			 << endl
+			 << "Il vous restera " << solde << " euros"
+			 << " à la rentrée." << endl
+			 << endl;
+		piggy.puiser(budget);
+	}
+	else
+	{
+		cout << "Il vous manque " << solde << " euros"
+			 << " pour partir en vacances !" << endl
+			 << endl;
+	}
+	return 0;
+}
